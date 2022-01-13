5edecccb6e1e6c188d142fb194e159c8f33a30f6
Vance <leesolinap@gmail.com>
2022-01-12 16:28:36 -0800

    Initial Commit

---
+++ GitHub learning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHub learning
+//
+//  Created by Melodee Solinap on 1/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub learning/GitHub_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_learningApp.swift
+//  GitHub learning
+//
+//  Created by Melodee Solinap on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
